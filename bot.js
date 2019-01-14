0a21f8f5407726464b3e7315e4564d9931c51ccc
BzbozAl3rbx <40595680+BzbozAl3rbx@users.noreply.github.com>
2019-01-14 14:06:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '!'
- 
- 
- 
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`strokers  `,"https://www.twitch.tv/dggamingbot")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
 });
- var prefix = "!"
+var prefix = "!"
 client.on('message', message => {
   if (message.author.xErenaa) return;
   if (!message.content.startsWith(prefix)) return;
@@ -244,20 +219,35 @@ if(!message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return mess
   }
  
 });
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-client.login(NTMzOTkwNTQwNjA2NDM5NDQ2.DxzFjg.vxdmh_Nmxn2iG9rVPeRjkH_j2mc);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('NTMzOTkwNTQwNjA2NDM5NDQ2.DxzFjg.vxdmh_Nmxn2iG9rVPeRjkH_j2mc');
